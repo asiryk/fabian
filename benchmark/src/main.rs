e511d60795e9b1f2bb5498fecbb81044a92f63cf
asiryk <asiryk.net@gmail.com>
2023-08-06 21:41:52 +0300

    feat: add method pick

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -3,6 +3,7 @@
 use search::naive;
 use search::rabin_karp;
 use memchr::memmem;
+use search::rabin_karp::HashingMethod;
 
 pub const WORDS_15000_RAW: &'static str = include_str!("../data/words-15000");
 
@@ -53,7 +54,7 @@ fn rabin_karp_huge_haystack_large_needle() -> Option<usize> {
     let haystack = WORDS_15000_RAW.as_bytes();
     let needle = "unprepossessing narcissist amazingly microelectronics sassafrases barnacled shepherding capitalists horsetail unintentional grandchildren predestines undifferentiated possesses attenuate passivization reconcilable carpetbag shortsightedly combative photoengrave flowerpots fallibility graveyard denominated checkrooms hollowest extemporize mathematics abundantly limescale supporting fascicles translucency diplomatic carthorse gridlocked emasculating unassailable thingamabob counterclaimed contribution overzealous fatherlands bracketed animosity virtualization enriching repudiated redactors commandeered consideration mortuaries chamberlains cuckolded perturbing flabbiness crankshaft analogizes desensitizing lasciviousness placeholder shareholder architraves redelivering monopolizes mythology cutthroats philanthropically premiered imprecisely unsealing alphabetizations crossbowman rectifier exquisitely overwhelms whatshisname contradistinction inadequacies helicoptered tealights capability understudies grumpiest jeeringly Zarathustra jellyfishes steaminess Menkalinan netiquettes purposelessly impounding pinpoints southwards dysphagia wholesalers existence microwaving dishonors ineligibles lavaliere unaesthetic inflexibly acknowledging unforgivable barracuda constrictive marveling insularity preconceptions fiddlesticks redounded strikebreaker theologically abidingly falsifying collocate predominately transposition Turkmenistan schnitzels Chittagong corroding colloquially underacted motorcycle mouthpiece haystacks margaritas antipodals givebacks phenomenon ultrasound salesperson stomachache bombarding sandpapering".as_bytes();
 
-    rabin_karp::search(haystack, needle)
+    rabin_karp::search(haystack, needle, HashingMethod::RabinFingerprint)
 }
 
 /// Benchmark 1: fabian-bench -w 20 \
@@ -73,7 +74,7 @@ fn rabin_karp_63_haystack() -> Option<usize> {
     let haystack = "limescale supporting fascicles translucency diplomatic carthorse".as_bytes();
     let needle = "carthorse".as_bytes();
 
-    rabin_karp::search(haystack, needle)
+    rabin_karp::search(haystack, needle, HashingMethod::RabinFingerprint)
 }
 
 /// Benchmark 1: fabian-bench -w 20 \
--- search/src/rabin_karp/mod.rs
+++ search/src/rabin_karp/mod.rs
@@ -1,6 +1,14 @@
 pub mod rabin_fingerprint;
 pub mod basic_hash;
 
-pub fn search(haystack: &[u8], needle: &[u8]) -> Option<usize> {
-    rabin_fingerprint::search(haystack, needle)
+pub enum HashingMethod {
+    Basic,
+    RabinFingerprint,
+}
+
+pub fn search(haystack: &[u8], needle: &[u8], method: HashingMethod) -> Option<usize> {
+    match method {
+        HashingMethod::Basic => basic_hash::search(haystack, needle),
+        HashingMethod::RabinFingerprint => rabin_fingerprint::search(haystack, needle),
+    }
 }
